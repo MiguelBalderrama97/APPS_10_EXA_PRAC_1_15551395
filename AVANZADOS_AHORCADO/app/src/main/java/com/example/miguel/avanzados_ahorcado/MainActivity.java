18e225b0926d58fc134fad24cad603aee66285ab
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-02-22 16:42:30 -0700

    Ahorcado_componentes
    Vinculación de algunos componentes y de la lista de palabras

--- AVANZADOS_AHORCADO/app/src/main/java/com/example/miguel/avanzados_ahorcado/MainActivity.java
+++ AVANZADOS_AHORCADO/app/src/main/java/com/example/miguel/avanzados_ahorcado/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.miguel.avanzados_ahorcado;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> palabras = new ArrayList<>();
+    private int position;
+
+    private TextView txtPalabra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        palabras = getAllWords();
+
+        position = (int)(Math.random() * 10);
+
+        txtPalabra = findViewById(R.id.txtPalabra);
+
+        txtPalabra.setText(palabras.get(position)+"");
+
+    }
+
+    private List<String> getAllWords(){
+        palabras.add("aplicacion");
+        palabras.add("moviles");
+        palabras.add("layout");
+        palabras.add("context");
+        palabras.add("intent");
+        palabras.add("toast");
+        palabras.add("alert");
+        palabras.add("activity");
+        palabras.add("list");
+        palabras.add("adapter");
+        palabras.add("listener");
+        return palabras;
+    }
+
+    private String convertir(String palabra){
+        for(int i = 0; i<palabra.length(); i++){
+            ;
+        }
     }
 }
